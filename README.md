654c9bb3b57a279cefe3fa3c3e344d0e3ad465a1
gmmarconi <gmmarconi@gmail.com>
2018-03-13 18:21:37 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # iCub-DeepLabSegmentation
 A Yarp module for istantiating the Deeplab V3+ on the humanoid robot iCub
+
+# Installation
+
+## Dependencies
+
+DeepLab depends on the following libraries:
+
+*   Numpy
+*   Pillow 1.0
+*   tf Slim (which is included in the "tensorflow/models/research/" checkout)
+*   Matplotlib
+*   Tensorflow
+
+For detailed steps to install Tensorflow, follow the [Tensorflow installation
+instructions](https://www.tensorflow.org/install/). A typical user can install
+Tensorflow using one of the following commands:
+```
+
+## Add Libraries to PYTHONPATH
+
+When running locally, the tensorflow/models/research/ and slim directories
+should be appended to PYTHONPATH. This can be done by running the following from
+tensorflow/models/research/:
+
+```bash
+# From tensorflow/models/research/
+export PYTHONPATH=$PYTHONPATH:`pwd`:`pwd`/slim
+```
+
+Note: This command needs to run from every new terminal you start. If you wish
+to avoid running this manually, you can add it as a new line to the end of your
+~/.bashrc file.
+
+# Testing the Installation
+
+You can test if you have successfully installed the Tensorflow DeepLab by
+running the following commands:
+
+Quick test by running model_test.py:
+
+```bash
+# From tensorflow/models/research/
+python deeplab/model_test.py
+```
+
+
---
+++ core/deeplab_evaluation.py
@@ -0,0 +1,69 @@
+import collections
+import sys
+from istantiate_model import DeepLabModel
+import get_dataset_colormap
+
+
+### Optional Visualization packages
+from matplotlib import gridspec
+from matplotlib import pyplot as plt
+import numpy as np
+###
+
+import tensorflow as tf
+
+if tf.__version__ < '1.5.0':
+    raise ImportError('Please upgrade your tensorflow installation to v1.5.0 or newer!')
+
+# Needed to show segmentation colormap labels
+############3
+# insted of 'ulils' put the path to the deeplab utils folder
+############
+
+
+model_dir = "/path/to/model/dir"
+_TARBALL_NAME = 'deeplab_model.tar.gz'
+
+
+model = DeepLabModel(model_dir)
+
+LABEL_NAMES = np.asarray([
+    'background', 'aeroplane', 'bicycle', 'bird', 'boat', 'bottle',
+    'bus', 'car', 'cat', 'chair', 'cow', 'diningtable', 'dog',
+    'horse', 'motorbike', 'person', 'pottedplant', 'sheep', 'sofa',
+    'train', 'tv'
+])
+
+FULL_LABEL_MAP = np.arange(len(LABEL_NAMES)).reshape(len(LABEL_NAMES), 1)
+FULL_COLOR_MAP = get_dataset_colormap.label_to_color_image(FULL_LABEL_MAP)
+
+
+def vis_segmentation(image, seg_map):
+    plt.figure(figsize=(15, 5))
+    grid_spec = gridspec.GridSpec(1, 4, width_ratios=[6, 6, 6, 1])
+
+    plt.subplot(grid_spec[0])
+    plt.imshow(image)
+    plt.axis('off')
+    plt.title('input image')
+
+    plt.subplot(grid_spec[1])
+    seg_image = get_dataset_colormap.label_to_color_image(
+        seg_map, get_dataset_colormap.get_pascal_name()).astype(np.uint8)
+    plt.imshow(seg_image)
+    plt.axis('off')
+    plt.title('segmentation map')
+
+    plt.subplot(grid_spec[2])
+    plt.imshow(image)
+    plt.imshow(seg_image, alpha=0.7)
+    plt.axis('off')
+    plt.title('segmentation overlay')
+
+    unique_labels = np.unique(seg_map)
+    ax = plt.subplot(grid_spec[3])
+    plt.imshow(FULL_COLOR_MAP[unique_labels].astype(np.uint8), interpolation='nearest')
+    ax.yaxis.tick_right()
+    plt.yticks(range(len(unique_labels)), LABEL_NAMES[unique_labels])
+    plt.xticks([], [])
+    ax.tick_params(width=0)
